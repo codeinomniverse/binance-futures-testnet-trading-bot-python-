b036fd18bb16f180ebd6d88f4876af87e9482daf
Rahul Kumar <shinushinu456@gmail.com>
2025-12-30 14:15:59 +0530

    set default futures leverage

--- bot/basic_bot.py
+++ bot/basic_bot.py
@@ -7,11 +7,21 @@ class BasicBot:
         # Logger setup
         self.logger = setup_logger()
 
-        # Binance client initialize
+        # Binance client init
         self.client = Client(api_key, api_secret)
 
         if testnet:
-            # Futures testnet URL set kar rahe hain
+            # Futures Testnet URL
             self.client.FUTURES_URL = BINANCE_TESTNET_URL
             self.logger.info("Connected to Binance Futures Testnet")
+
+        # Futures leverage set (IMPORTANT)
+        try:
+            self.client.futures_change_leverage(
+                symbol="BTCUSDT",
+                leverage=1
+            )
+            self.logger.info("Leverage set to 1x for BTCUSDT")
+        except Exception as e:
+            self.logger.error(f"Error setting leverage: {str(e)}")
 
